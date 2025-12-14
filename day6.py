3ce8584e3ed37a8d23c282e2b52b2bdc4b59b413
bazbrad765 <bazbrad@gmail.com>
2025-12-14 12:46:40 -0800

    failed day6

--- day6.py
+++ day6.py
@@ -1,12 +1,13 @@
 import numpy as np
 import re
 
-##FAILED!
+#FAILED
 
 lights_array = np.zeros((1000,1000), dtype=bool)
-count = 0
-def process_lights(t: str):
+line_num = 1
 
+def process_lights(t: str):
+    global line_num
     start_digits = re.findall(r"\d+(?=,)",t)
     start_digits = int(start_digits[0])
     end_digits = re.findall(r"\d+",t)
@@ -17,33 +18,51 @@ def process_lights(t: str):
     end_digits2 = re.findall(r"\d+",t)
     end_digits2 = int(end_digits2[3])
 
-    print("Start Numbers: " +str(start_digits),str(end_digits))
-    print("End Numbers: " +str(start_digits2),str(end_digits2))
 
+    print("Line # " , str(line_num))
+    print("Instructions: " ,t)
     if t[1] == "u":
-        if t[6] == "n":
-            for light in lights_array[start_digits:end_digits],[start_digits2:end_digits2]:
-                light = True
-                print(light)
-                #print("turned light on SB")
+        if t[6] == "n": #turn on
+            print("New Lights being tunrned ON")
+            
+            print("Start Numbers: " +str(start_digits),str(end_digits))
+
+            print("End Numbers: " +str(start_digits2),str(end_digits2))
+            
+
+            lights_array[start_digits:end_digits,start_digits2:end_digits2] = True
+
+
         else:
+            print("New Lights being tunrned OFF")
+            print("Start Numbers: " +str(start_digits),str(end_digits))
 
-            for light in lights_array[start_digits:end_digits,start_digits2:end_digits2]:
-                light = False
-                #print("turned light off")
+            print("End Numbers: " +str(start_digits2),str(end_digits2))
+            
+            lights_array[start_digits:end_digits,start_digits2:end_digits2] = False
+
     else:
-        for light in lights_array[start_digits:end_digits,start_digits2:end_digits2]:
-            if light == True:
-                light = False
-            else:
-                light = True
+        print("New Lights being TOGGLED")
+        print("Start Numbers: " +str(start_digits),str(end_digits))
 
+        print("End Numbers: " +str(start_digits2),str(end_digits2))
+        
+        lights_array[start_digits:end_digits,start_digits2:end_digits2] = False if True else True
+
+    print("\n")
+    line_num += 1
 
 
 with open("input6.txt", "r") as f:
     for line in f:
-        print(line)
         process_lights(line)
+
+count = 0
+for i in lights_array:
+    for a in i:
+        if a == True:
+            count += 1
+
 print(count)
 
 
